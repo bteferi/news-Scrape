119bb04094c7b9ada1de0174bf3db9c4480b2ad3
Beza <bezawit.teferi@gmail.com>
2018-04-04 21:35:17 -0400

    restructure mvc

--- README.md
+++ README.md
@@ -5,27 +5,14 @@ A web app that lets users view and leave comments on the latest news.
 ## File Structure
 
 ```
-├── controllers
-|  ├── fetch.js
-|  ├── headline.js
-|  └── note.js
 ├── models
 |  ├── Headline.js
 |  ├── index.js
 |  └── Note.js
 ├── public
-|  └── assets
-├── routes
-|  ├── api
-|  ├── index.js
-|  └── view
-├── scripts
-|  └── scrape.js
-└── views
-|   ├── home.handlebars
-|   ├── layouts
-|   └── saved.handlebars
-├── package-lock.json
+|  ├── headline.js
+|  └── index.html
+├── .gitignore
 ├── package.json
 └── server.js
 ```
--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var app = express();
 
 // Database configuration
 var databaseUrl = "nytArticles";
-var collections = ["nytData"];
+var collections = ["Headline"];
 
 // Hook mongojs configuration to the db variable - from npm docs
 var db = mongojs(databaseUrl, collections);
@@ -18,8 +18,8 @@ db.on("error", function(error) {
   console.log("Database Error:", error);
 });
 
-app.get("/", function (req, res){
-    res.send("Hello World")
+app.get("/", function(req, res) {
+  res.send("Hello World")
 });
 
 // route 1
@@ -29,7 +29,7 @@ app.get("/", function (req, res){
 // find everything - do 20 articles
 app.get("/all", function(req, res) {
   // Find all results from the scrapedData collection in the db
-  db.nytData.find({}, function(error, found) {
+  db.Headline.find({}, function(error, found) {
     // Throw any errors to the console
     if (error) {
       console.log(error);
@@ -45,54 +45,39 @@ app.get("/all", function(req, res) {
 // Scrape data from one site and place it into the mongodb db
 //=================
 
-app.get("/scrape", function (req, res){
+app.get("/scrape", function(req, res) {
 
-  request ("https://www.nytimes.com/", function (error, response, html){
+  request("https://www.nytimes.com/", function(error, response, html) {
 
-      var $ = cheerio.load(html);
+    var $ = cheerio.load(html);
 
-      $(".story-heading").each(function(i, element) {
-            var title = $(this).children("a").text();
-            var link = $(this).children("a").attr("href");
-//==================if they both exist save in to db ===================
-              if (title && link) {
-                // Insert the data in the scrapedData db
-                db.nytData.insert({
-                  title: title,
-                  link: link
-                },
-                function(err, saved) {
-                  if (err) {
-                    // Log the error if one is encountered during the query
-            console.log(err);
-          }
-            else {
+    $(".story-heading").each(function(i, element) {
+      var title = $(this).children("a").text();
+      var link = $(this).children("a").attr("href");
+      //==================if they both exist save in to db ===================
+      if (title && link) {
+        // Insert the data in the scrapedData db
+        db.Headline.insert({
+            title: title,
+            link: link
+          },
+          function(err, saved) {
+            if (err) {
+              // Log the error if one is encountered during the query
+              console.log(err);
+            } else {
               // Otherwise, log the inserted data
               console.log(saved);
             }
-});
+          });
 
 
+      }
 
-
-
-
-
-              }
-
-      });
+    });
   })
   res.send("NYT Scrape Complete");
 })
-
-
-
-
-
-
-
-
-
 
 
 
