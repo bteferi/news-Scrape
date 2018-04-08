eff568fcb96814a269f748152e7cb4d9ccef4011
Beza <bezawit.teferi@gmail.com>
2018-04-07 21:10:49 -0400

    link is available

--- public/app.js
+++ public/app.js
@@ -3,14 +3,16 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < 5; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p class=border border-dark card-body>"  + data[i].title +  data[i].link + "<a href='" + data[i].link + "'>"+ "<br />"  +"<button class=btn-default data-save='" + data[i]._id + "'> Save </button>" + "<button class=btn-danger data-delete='" + data[i]._id + "'> Delete </button>"+ "</p>" + "<br>");
+    $("#articles").append( "<div  class=border border-dark card-body>" +
+    "<p>"  + data[i].title +"</p>" +
+      "<p>" +
+    "<a href='" + data[i].link + "'>  Click here to read more </a>"+ "<br>" +
+      "<p>" +
+    "<p>" + "<br />"  +"<button class=btn-default data-save='" + data[i]._id + "'> Save </button>" + "<button class=btn-danger data-delete='" + data[i]._id + "'> Delete </button>" +
+
+    "</div>" +
+    "<br>");
   }
 });
 
-// <a href=" data[i].link" class="card-link">Card link</a>
-//
-// "<a href=" data[i].link" '>"
-// "<a href='" + data[i]._id + "'>"
-
-
-// + "<a href='" + data[i].link + "'>"
+// <a href="data[i].link" >data[i].link</a>
