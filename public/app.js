f312437517c6ee516ed96e11188b24db5d803d4c
Beza <bezawit.teferi@gmail.com>
2018-04-07 20:41:15 -0400

    display - look and feel

--- public/app.js
+++ public/app.js
@@ -3,8 +3,14 @@ $.getJSON("/articles", function(data) {
   // For each one
   for (var i = 0; i < 5; i++) {
     // Display the apropos information on the page
-    $("#articles").append("<p>" + data[i].title + "<br />" + data[i].link +"<button data-edit='" + data[i]._id + "'>" + "<button data-delete='" + data[i]._id + "'>" + "</p>");
+    $("#articles").append("<p class=border border-dark card-body>"  + data[i].title +  data[i].link + "<a href='" + data[i].link + "'>"+ "<br />"  +"<button class=btn-default data-save='" + data[i]._id + "'> Save </button>" + "<button class=btn-danger data-delete='" + data[i]._id + "'> Delete </button>"+ "</p>" + "<br>");
   }
 });
-// start here -
-// "<button data-id='" + data[i]._id + "'>"
+
+// <a href=" data[i].link" class="card-link">Card link</a>
+//
+// "<a href=" data[i].link" '>"
+// "<a href='" + data[i]._id + "'>"
+
+
+// + "<a href='" + data[i].link + "'>"
